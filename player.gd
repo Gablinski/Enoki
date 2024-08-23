9b09122a906f795d421348375f3f0dbf973e261e
Gablinski <gabsil37@kingshigh.school.nz>
2024-08-23 13:21:19 +1200

    V9

--- player.gd
+++ player.gd
@@ -4,6 +4,12 @@ var speed = 100
 
 var player_state
 
+
+
+var bow_equppied = true
+var bow_cooldown = true
+var arrow = preload("res://arrow.tscn")
+
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	
@@ -14,6 +20,17 @@ func _physics_process(delta):
 	
 	velocity = direction * speed
 	move_and_slide()
+	
+	var mouse_pos = get_global_mouse_position()
+	$Marker2D.look_at(mouse_pos)
+	
+	if Input.is_action_just_pressed("leftmouse") and bow_equppied and bow_cooldown:
+		bow_cooldown = false
+		var arrow_instance =  arrow.instantiate()
+		arrow_instance.rotation = $Marker2D.rotation
+		arrow_instance.global_position = $Marker2D.global_position
+		add_child(arrow_instance)
+		
 	
 	play_anim(direction)
 
